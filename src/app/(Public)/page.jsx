f928375e7305541970043e6819e648a369996c2f
novrialsandi <novrialsandi@gmail.com>
2025-05-11 23:51:14 +0700

    Implement landing page with dashboard link and styling

--- src/app/(Public)/page.jsx
+++ src/app/(Public)/page.jsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 const LandingPage = () => {
-	return <div>LandingPage</div>;
+	return (
+		<div className="w-full h-screen flex justify-center items-center">
+			<Link href="/dashboard" className="p-2 bg-primary text-white rounded-md">
+				Go to Dashboard
+			</Link>
+		</div>
+	);
 };
 
 export default LandingPage;
