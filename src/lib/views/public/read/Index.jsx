e51d79fb95a89e4e20c87e85ac4b2c4985ffc3dc
novrialsandi <novrialsandi@gmail.com>
2025-05-16 14:31:01 +0700

    feat: wrap Collections component in Suspense for improved loading handling

--- src/lib/views/public/read/Index.jsx
+++ src/lib/views/public/read/Index.jsx
@@ -1,9 +1,14 @@
+"use client";
+
+import { Suspense } from "react";
 import Collections from "./Collections";
 
 const Read = () => {
 	return (
 		<div className="p-6 max-w-4xl w-full">
-			<Collections />
+			<Suspense>
+				<Collections />
+			</Suspense>{" "}
 		</div>
 	);
 };
