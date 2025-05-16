f9c8705ef441bf5495f7e0468b756e484032c9d7
novrialsandi <novrialsandi@gmail.com>
2025-05-16 10:29:31 +0700

    fix: correct component usage in PublicLayout and update return statement in Index component

--- src/app/(Public)/layout.jsx
+++ src/app/(Public)/layout.jsx
@@ -1,12 +1,12 @@
-import PublicLayout from "@/lib/layout/public/Index";
+import Layout from "@/lib/layout/public/Index";
 // import { GoogleAnalytics } from "@next/third-parties/google";
 
 const PublicLayout = ({ children }) => {
 	return (
-		<PublicLayout>
+		<Layout>
 			{children}
 			{/* <GoogleAnalytics gaId="G-920REJG2GY" /> */}
-		</PublicLayout>
+		</Layout>
 	);
 };
 
--- src/lib/layout/public/Index.jsx
+++ src/lib/layout/public/Index.jsx
@@ -1,5 +1,5 @@
 const PublicLayout = ({ children }) => {
-	return { children };
+	return <>{children}</>;
 };
 
 export default PublicLayout;
