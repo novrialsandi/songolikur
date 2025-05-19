38684f4b16e5e56e5a36e4c32471e66431672118
novrialsandi <novrialsandi@gmail.com>
2025-05-19 10:39:01 +0700

    feat: implement API route handlers for dynamic path handling

--- src/lib/api/fetchApi.js
+++ src/lib/api/fetchApi.js
@@ -1,20 +1,34 @@
 import axios from "axios";
 import { getCookie } from "../helpers/cookie";
 
+// const fetchApi = axios.create({
+// 	baseURL: `${process.env.NEXT_PUBLIC_SERVICE_HOST}/api`,
+// });
+
+// fetchApi.interceptors.request.use((config) => {
+// 	const token = getCookie("sid");
+// 	const secretKey = process.env.NEXT_PUBLIC_SECRET_KEY;
+
+// 	if (token) {
+// 		config.headers.Authorization = `Bearer ${token}`;
+// 	}
+
+// 	if (secretKey) {
+// 		config.headers["x-secret-key"] = secretKey;
+// 	}
+
+// 	return config;
+// });
+
 const fetchApi = axios.create({
-	baseURL: `${process.env.NEXT_PUBLIC_SERVICE_HOST}/api`,
+	baseURL: "/api",
 });
 
 fetchApi.interceptors.request.use((config) => {
 	const token = getCookie("sid");
-	const secretKey = process.env.NEXT_PUBLIC_SECRET_KEY;
 
 	if (token) {
 		config.headers.Authorization = `Bearer ${token}`;
-	}
-
-	if (secretKey) {
-		config.headers["x-secret-key"] = secretKey;
 	}
 
 	return config;
