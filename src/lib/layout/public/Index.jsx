fd94cb65fc4601935861d22ba7a0bc001e2c8911
novrialsandi <novrialsandi@gmail.com>
2025-05-22 16:48:06 +0700

    fix: adjust padding in PublicLayout for better responsiveness

--- src/lib/layout/public/Index.jsx
+++ src/lib/layout/public/Index.jsx
@@ -3,10 +3,9 @@ import HeaderPublic from "./Header";
 const PublicLayout = ({ children }) => {
 	return (
 		<div className="flex w-full justify-center">
-			<div className="relative py-8 px-20 space-y-12 w-full max-w-[1440px]">
+			<div className="relative py-8 md:px-28 px-4 space-y-12 w-full max-w-[1440px]">
 				<HeaderPublic />
 
-				{children}
 				{children}
 			</div>
 		</div>
