b4a3dabb5823febe5152ac421bc64c97ef6bdc1e
novrialsandi <novrialsandi@gmail.com>
2025-05-30 23:00:43 +0700

    refactor: enhance Hero component to fetch and display collections with improved loading state

--- src/lib/views/public/Hero.jsx
+++ src/lib/views/public/Hero.jsx
@@ -1,27 +1,79 @@
+"use client";
+
 import Divider from "@/lib/components/Divider";
 import React from "react";
+import { useState, useEffect } from "react";
+import fetchApi from "@/lib/api/fetchApi";
+import Link from "next/link";
 
 const Hero = () => {
+	const [collections, setCollections] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	const getCollections = async () => {
+		setLoading(true);
+		const res = await fetchApi("/collection/public");
+
+		if (res.status === 200) {
+			const repeatedData = [];
+
+			for (let i = 0; i < 5; i++) {
+				repeatedData.push(...res.data); // Spread to flatten
+			}
+
+			setCollections(repeatedData);
+		}
+		setLoading(false);
+	};
+
+	console.log(collections);
+
+	useEffect(() => {
+		getCollections();
+	}, []);
+
 	return (
-		<div className="flex h-screen gap-8">
-			<div className="flex w-1/4">
-				<div>
-					<img className="aspect-video" src="/placeholder.webp" alt="gambar" />
+		<>
+			{!loading && (
+				<div className="flex h-screen gap-8">
+					<div className="flex w-1/4">
+						<div>
+							<img
+								className="aspect-video"
+								src="/placeholder.webp"
+								alt="gambar"
+							/>
+						</div>
+					</div>
+
+					<Divider orientation="vertical" />
+
+					<div className="flex w-2/4">
+						<div>
+							<Link href={`/read/${collections[0].slug}`}>
+								<img
+									className="aspect-video  object-cover rounded-md"
+									src={collections[0].thumbnail || "/placeholder.webp"}
+									alt="gambar"
+								/>
+							</Link>
+						</div>
+					</div>
+
+					<Divider orientation="vertical" />
+
+					<div className="flex w-1/4">
+						<div>
+							<img
+								className="aspect-video"
+								src="/placeholder.webp"
+								alt="gambar"
+							/>
+						</div>
+					</div>
 				</div>
-			</div>
-			<Divider orientation="vertical" />
-			<div className="flex w-2/4">
-				<div>
-					<img className="aspect-video" src="/placeholder.webp" alt="gambar" />
-				</div>
-			</div>
-			<Divider orientation="vertical" />
-			<div className="flex w-1/4">
-				<div>
-					<img className="aspect-video" src="/placeholder.webp" alt="gambar" />
-				</div>
-			</div>
-		</div>
+			)}
+		</>
 	);
 };
 
--- src/lib/views/public/read/slug/Index.jsx
+++ src/lib/views/public/read/slug/Index.jsx
@@ -41,10 +41,24 @@ const SlugComponent = ({ data, slug }) => {
 
 	return (
 		<div className="p-6 w-full flex justify-center">
-			<div className="max-w-3xl">
+			<div className="max-w-3xl space-y-6">
 				{data.thumbnail && (
 					<img src={data.thumbnail} alt={data.title || "Article thumbnail"} />
 				)}
+				<div className="text-center text-3xl">{data.title}</div>
+
+				<div className="flex justify-center items-center gap-4">
+					<img
+						className="size-8 rounded-full"
+						src={data.user.avatar || "/avatar.png"}
+						alt=""
+					/>
+					<div>
+						<div>{data.user.name}</div>
+						<div className="text-xs">ini title</div>
+					</div>
+				</div>
+
 				<div dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
 			</div>
 		</div>
